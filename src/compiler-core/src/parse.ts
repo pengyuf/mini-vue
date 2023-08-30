b034bf8a3a5f7d4617fcee3d3c6619fba6cc1775
best <2907647445@qq.com>
2023-08-30 20:25:05 +0800

    解析三种联合类型

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -4,37 +4,71 @@ export function baseParse(content: string) {
 
     const context = createParseContext(content)
 
-    return createRoot(parseChildren(context))
+    return createRoot(parseChildren(context, []))
 }
 
 
-function parseChildren(context: any) {
+function parseChildren(context: any, ancestors) {
     const nodes: any[] = []
-    let node
-    const s = context.source
-    if (s.startsWith('{{')) {
-        //  解析插值
-        node = parseInterpolation(context)
-    } else if (s[0] === '<') {
-        if (/[a-z]/i.test(s[1])) {
-            // 解析element
-            node = parseElement(context)
+
+    while (!isEnd(context, ancestors)) {
+        let node
+        const s = context.source
+        if (s.startsWith('{{')) {
+            //  解析插值
+            node = parseInterpolation(context)
+        } else if (s[0] === '<') {
+            if (/[a-z]/i.test(s[1])) {
+                // 解析element
+                node = parseElement(context, ancestors)
+            }
         }
+
+        if (!node) {
+            // 默认当做text处理
+            node = parseText(context)
+        }
+        nodes.push(node)
     }
-
-    if (!node) {
-        // 默认当做text处理
-        node = parseText(context)
-    }
-
-    nodes.push(node)
-
     return nodes
 }
 
 
+function isEnd(context, ancestors) {
+    const s = context.source
+
+    if (s.startsWith('</')) {
+        for (let i = ancestors.length-1; i >= 0; i--) {
+            const tag = ancestors[i].tag
+            if (startsWithEndTagOpen(s,tag)) {
+                return true
+            }
+        }
+    }
+
+    // if (parentTag && s.startsWith(`</${parentTag}>`)) {
+    //     return true
+    // }
+
+    return !s
+}
+
+
 function parseText(context: any) {
-    const content = parseTextData(context, context.source.length)
+    let endIndex = context.source.length
+    let endTokens = ["<", "{{"]
+
+    for (let i = 0; i < endTokens.length; i++) {
+        const index = context.source.indexOf(endTokens[i])
+        if (index !== -1 && endIndex > index) {
+            endIndex = index
+        }
+    }
+
+
+
+    const content = parseTextData(context, endIndex)
+    console.log('content------------', content)
     return {
         type: NodeTypes.TEXT,
         content
@@ -52,10 +86,22 @@ const enum TagType {
     End
 }
 
-function parseElement(context: any) {
-    const element = parseTag(context, TagType.Start)
-    parseTag(context, TagType.End)
+function parseElement(context: any, ancestors) {
+    const element: any = parseTag(context, TagType.Start)
+    ancestors.push(element)
+    element.children = parseChildren(context, ancestors)
+    ancestors.pop()
+    if (startsWithEndTagOpen(context.source, element.tag)) {
+        parseTag(context, TagType.End)
+    } else {
+        throw new Error(`缺少结束标签:${element.tag}`)
+    }
     return element
+}
+
+
+function startsWithEndTagOpen(source, tag) {
+    return source.startsWith('</') && source.slice(2, 2 + tag.length).toLowerCase() === tag.toLowerCase()
 }
 
 function parseTag(context: any, type: TagType) {
--- src/compiler-core/test/parse.spec.ts
+++ src/compiler-core/test/parse.spec.ts
@@ -22,7 +22,8 @@ describe("Parse", () => {
             const ast = baseParse("<div></div>")
             expect(ast.children[0]).toStrictEqual({
                 type: NodeTypes.ELEMENT,
-                tag:'div'
+                tag: 'div',
+                children: []
             })
         })
     })
@@ -34,9 +35,66 @@ describe("Parse", () => {
             const ast = baseParse("some text")
             expect(ast.children[0]).toStrictEqual({
                 type: NodeTypes.TEXT,
-                content:'some text'
+                content: 'some text'
             })
         })
     })
-    
+
+
+    test('hello wrold', () => {
+        const ast = baseParse('<p>hi,{{message}}</p>')
+        expect(ast.children[0]).toStrictEqual({
+            type: NodeTypes.ELEMENT,
+            tag: 'p',
+            children: [
+                {
+                    type: NodeTypes.TEXT,
+                    content: 'hi,'
+                },
+                {
+                    type: NodeTypes.INTERPOLATION,
+                    content: {
+                        type: NodeTypes.SIMPLE_EXPRESSION,
+                        content: "message"
+                    }
+                }
+            ]
+        })
+    })
+
+
+    test('nested element', () => {
+        const ast = baseParse('<div><p>hi</p>{{message}}</div>')
+        expect(ast.children[0]).toStrictEqual({
+            type: NodeTypes.ELEMENT,
+            tag: 'div',
+            children: [
+                {
+                    type: NodeTypes.ELEMENT,
+                    tag: 'p',
+                    children: [
+                        {
+                            type: NodeTypes.TEXT,
+                            content: 'hi'
+                        }
+                    ]
+                },
+                {
+                    type: NodeTypes.INTERPOLATION,
+                    content: {
+                        type: NodeTypes.SIMPLE_EXPRESSION,
+                        content: "message"
+                    }
+                }
+            ]
+        })
+    })
+
+
+    test('throw error', () => {
+        expect(() => {
+            baseParse('<div><span></div>')
+        }).toThrow('缺少结束标签:span')
+    })
+
 })
