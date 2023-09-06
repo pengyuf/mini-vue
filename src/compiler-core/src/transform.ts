5f83386c82e1390a654a7955ef3c13200715c462
best <2907647445@qq.com>
2023-09-06 16:23:28 +0800

    联合三种类型，实现render

--- src/compiler-core/src/transform.ts
+++ src/compiler-core/src/transform.ts
@@ -11,7 +11,12 @@ export function transform(root, options = {}) {
 }
 
 function createRootCodegen(root) {
-    root.codegenNode = root.children[0]
+    const child = root.children[0]
+    if (child.type == NodeTypes.ELEMENT) {
+        root.codegenNode = child.codegenNode
+    } else {
+        root.codegenNode = root.children[0]
+    }
 }
 
 function createTransfromContext(root, options) {
@@ -29,9 +34,13 @@ function createTransfromContext(root, options) {
 function traverseNode(node: any, context) {
 
     const nodeTransforms = context.nodeTransforms
+    const exitFns: any = []
     for (let i = 0; i < nodeTransforms.length; i++) {
         const transform = nodeTransforms[i];
-        transform(node,context)
+        const onExit = transform(node, context)
+        if (onExit) {
+            exitFns.push(onExit)
+        }
     }
 
 
@@ -45,6 +54,12 @@ function traverseNode(node: any, context) {
             break;
         default:
             break;
+    }
+
+
+    let i = exitFns.length
+    while (i--) {
+        exitFns[i]()
     }
 
 }
--- src/compiler-core/src/transforms/transformElement.ts
+++ src/compiler-core/src/transforms/transformElement.ts
@@ -1,8 +1,17 @@
-import { NodeTypes } from "../ast";
+import { createVNodeCall, NodeTypes } from "../ast";
 import { CREATE_ELEMENT_VNODE } from "../runtimeHelpers";
 
-export function transformElement(node:any,context:any){
-    if(node.type == NodeTypes.ELEMENT){
-        context.helper(CREATE_ELEMENT_VNODE)
+export function transformElement(node: any, context: any) {
+    if (node.type == NodeTypes.ELEMENT) {
+
+        return () => {
+
+            const vnodeTag = `'${node.tag}'`
+            let vnodeProps
+            const children = node.children
+            let vnodeChildren = children[0]
+
+            node.codegenNode = createVNodeCall(context, vnodeTag, vnodeProps, vnodeChildren)
+        }
     }
 }
