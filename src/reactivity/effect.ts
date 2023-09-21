eec376b4785c7d5e7bc69b59115c982a40355070
best <2907647445@qq.com>
2023-09-21 20:34:44 +0800

    实现effect相关功能

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,20 +1,51 @@
+import { extend } from "../shared";
+
 let activeEffect // 一个全局变量，在track中收集到targetMap中
 let targetMap = new Map()  // 存储effect的容器
 
 class ReactiveEffect {
     private _fn: any;
-    constructor(fn) {
+    public scheduler: any;
+    deps = []
+    active: any = true;
+    onStop: any;
+    constructor(fn, scheduler?) {
         this._fn = fn
+        this.scheduler = scheduler
     }
     run() {
         activeEffect = this
-        this._fn()
+        const result = this._fn()
+        return result
+    }
+    stop() {
+        if (this.active) {
+            clearEffect(this)
+            if (this.onStop) {
+                this.onStop()
+            }
+            this.active = false
+        }
     }
 }
 
-export function effect(fn) {
-    const _effect = new ReactiveEffect(fn)
+function clearEffect(effect) {
+    effect.deps.forEach((dep: any) => {
+        dep.delete(effect)
+    })
+}
+
+export function stop(runner) {
+    runner.effect.stop()
+}
+
+export function effect(fn, options: any = {}) {
+    const _effect = new ReactiveEffect(fn, options.scheduler)
+    extend(_effect, options)
     _effect.run()
+    const runner: any = _effect.run.bind(_effect)
+    runner.effect = _effect
+    return runner
 }
 
 // 依赖收集
@@ -29,7 +60,9 @@ export function track(target, key) {
         dep = new Set()
         depsMap.set(key, dep)
     }
+    if(!activeEffect)return
     dep.add(activeEffect)
+    activeEffect.deps.push(dep)
 }
 
 // 触发依赖
@@ -38,6 +71,10 @@ export function trigger(target, key) {
     let dep = depsMap.get(key)
 
     for (const effect of dep) {
+        if (effect.scheduler) {
+            effect.scheduler()
+            return
+        }
         effect.run()
     }
 }
--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -1,5 +1,5 @@
 import { reactive } from "../reactive"
-import {effect} from "../effect"
+import { effect,stop } from "../effect"
 
 describe('effect', () => {
     it('happy path', () => {
@@ -9,5 +9,66 @@ describe('effect', () => {
         expect(newAge).toBe(2)
         user.age++
         expect(newAge).toBe(3)
+    })
+
+    it('runner', () => {
+        let foo = 10
+        const runner = effect(() => {
+            foo++
+            return 'runner'
+        })
+        expect(foo).toBe(11)
+        const data = runner()
+        expect(foo).toBe(12)
+        expect(data).toBe('runner')
+    })
+
+    it('scheduler',()=>{
+        let dummy
+        let run:any
+        const scheduler = jest.fn(()=>{
+            run = runner
+        })
+        const obj = reactive({foo:1})
+        const runner = effect(()=>{
+            dummy = obj.foo
+        },{
+            scheduler
+        })
+        expect(scheduler).not.toHaveBeenCalled()
+        expect(dummy).toBe(1)
+        obj.foo++
+        expect(scheduler).toHaveBeenCalledTimes(1)
+        expect(dummy).toBe(1)
+        run()
+        expect(dummy).toBe(2)
+    })
+
+    it('stop',()=>{
+        let dummy
+        const obj = reactive({foo:1})
+        const runner = effect(()=>{
+            dummy = obj.foo
+        })
+        obj.foo = 2
+        expect(dummy).toBe(2)
+        stop(runner)
+        obj.foo = 3
+        expect(dummy).toBe(2)
+        runner()
+        expect(dummy).toBe(3)
+    })
+
+    it('onStop',()=>{
+        let dummy
+        const onStop = jest.fn()
+        const obj = reactive({foo:1})
+        const runner = effect(()=>{
+            dummy = obj.foo
+        },{
+            onStop
+        })
+        stop(runner)
+        expect(onStop).toBeCalledTimes(1)
     })
 })
